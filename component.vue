678734b082f51f84e2dab4eda425a1370caadb79
Jack <jackchappellfreelance@gmail.com>
2020-06-03 16:23:43 +1000

    base component

---
+++ component.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h3 v-if="meta.label">{{ meta.label }}</h3>
+        <md-field>
+            <label>Font</label>
+            <md-input v-model="value"></md-input>
+        </md-field>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    props: {
+        value: {
+            default: '',
+            type: String,
+        },
+        meta: Object,
+    }
+};
+</script>
+
+<style>
+
+</style>
